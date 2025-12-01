3d19df0f48fd3c22f05fd69e17064d99ec983f24
hassaammgl <h455a4mmehtab@gmail.com>
2025-12-01 20:34:50 +0500

    chore: Update developer's profile link in footer from GitHub to LinkedIn.

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -164,7 +164,7 @@ const Footer = () => {
         </p>
         <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
           <li className="text-white">
-            Developed by <a href="https://github.com/hassaammgl" className="text-primary font-bold">hassaammgl</a>
+            Developed by <a href="www.linkedin.com/in/m-hassaam-mughal-91668a256" className="text-primary font-bold">hassaammgl</a>
           </li>
         </ul>
       </div>
